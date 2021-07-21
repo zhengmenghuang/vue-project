5516cd83c8cb856e9e302cdc9737099c7a596a88
llxhzm <1481010383@qq.com>
2021-07-21 11:20:02 +0800

    feat: init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
 
 @Options({
   components: {
