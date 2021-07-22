1f90198ac9b1a7d34d6943ef6b919b141d04bb78
llxhzm <1481010383@qq.com>
2021-07-22 19:32:52 +0800

    feat: 路由相应拦截 完成

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 
 import { register } from 'register-service-worker';
-import axios, { AxiosRequestConfig } from 'axios';
+import axios from 'axios';
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
@@ -35,19 +35,21 @@ if (process.env.NODE_ENV === 'production') {
 // 在这里挂载 axios 的请求拦截
 axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 
-axios.interceptors.request.use((config: AxiosRequestConfig) => {
+axios.interceptors.request.use(config => {
   // 在发送请求之前做些什么
   config.headers.Authorization = 'test';
   return config;
 }, error => Promise.reject(error));
 
 // 添加响应拦截器
-// axios.interceptors.response.use(function (response) {
-//   // 2xx 范围内的状态码都会触发该函数。
-//   // 对响应数据做点什么
-//   return response;
-// }, function (error) {
-//   // 超出 2xx 范围的状态码都会触发该函数。
-//   // 对响应错误做点什么
-//   return Promise.reject(error);
-// });
+axios.interceptors.response.use(response => {
+  // 2xx 范围内的状态码都会触发该函数。
+  // 对响应数据做点什么
+  console.log(48, response);
+  return response;
+}, error => {
+  // 超出 2xx 范围的状态码都会触发该函数。
+  // 对响应错误做点什么
+  console.log(53, error.toString());
+  return Promise.reject(error);
+});
